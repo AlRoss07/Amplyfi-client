773e57e38ab2b2d6442a151cbaf0bf8abf81b54f
AlexXploy <145038676+AlexXploy@users.noreply.github.com>
2023-12-28 16:01:35 -0400

    Added Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplyficlient7f46935f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
