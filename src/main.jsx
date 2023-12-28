73a5266b4afb317c113586f668b1dd774ce58049
AlexXploy <145038676+AlexXploy@users.noreply.github.com>
2023-12-28 16:13:18 -0400

    Added Auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Amplify } from "aws-amplify";
 import './index.css';
-import config from './aws-exports.jsx';
+import config from './aws-exports.js';
 Amplify.configure(config)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
